8d4af6f5a16b28b5a318e6b89b7f38119f2d8e03
Theo Luttrell <142305221+theo-luttrell@users.noreply.github.com>
2025-10-15 18:57:42 +1100

    Update main.cpp

--- templatesQuiz/main.cpp
+++ templatesQuiz/main.cpp
@@ -16,6 +16,9 @@ int main()
 	std::cout << add(1.2, 3.4) << '\n';
 	std::cout << mult(2, 3) << '\n';
 	std::cout << mult(1.2, 3) << '\n';
+	std::cout << sub(3, 2) << '\n';
+	std::cout << sub(3.5, 2) << '\n';
+	std::cout << sub(4, 1.5) << '\n';
 
 	return 0;
 }
